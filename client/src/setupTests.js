1bbd65f42ae662766195c18170619daba5ace3c1
Ananto Ghosh <a0ghosh@gmail.com>
2018-05-08 17:15:32 +0530

    Throw error on prop type validation failure with jest

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,13 @@ import Adapter from "enzyme-adapter-react-16";
 import "jest-enzyme";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// eslint-disable-next-line no-console
+const error = console.error;
+// eslint-disable-next-line no-console
+console.error = (warning, ...args) => {
+  if (/(Invalid prop|Failed prop type)/gi.test(warning)) {
+    throw new Error(warning);
+  }
+  error.apply(console, [warning, ...args]);
+};
